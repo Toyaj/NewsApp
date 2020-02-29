c6ca3a9ed8877b335e02a5a465ef714c3473f517
Toyaj Nigam <toyaj.nigam@chimpfund.com>
2020-02-29 14:16:08 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Toyaj Nigam on 29/02/20.
+//  Copyright © 2020 Toyaj Nigam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
